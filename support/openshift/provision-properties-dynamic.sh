c64b11bc8cb8fc369bb63e11569a60a67b54869a
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2019-02-07 11:51:24 -0500

    Update provision-properties-dynamic.sh

--- support/openshift/provision-properties-dynamic.sh
+++ support/openshift/provision-properties-dynamic.sh
@@ -1,2 +1,2 @@
 PRJ_SUFFIX=${ARG_PROJECT_SUFFIX:-`echo $OPENSHIFT_USER | sed -e 's/[^-a-z0-9]/-/g'`}
-PRJ=("$PRJ_NAME" "RHPAM7 Order IT Hardware Demo" "$PRJ_DESCRIPTION")
+PRJ=("$PRJ_NAME" "Credit Card Fraud Detection" "$PRJ_DESCRIPTION")
