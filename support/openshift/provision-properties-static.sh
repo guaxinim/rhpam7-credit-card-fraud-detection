60ac116093f58ef4aa863cae86ff11a5d9c5a636
Elvis Rocha <elvishrocha@gmail.com>
2019-05-16 21:30:00 -0300

    corrigindo dados do projeto

--- support/openshift/provision-properties-static.sh
+++ support/openshift/provision-properties-static.sh
@@ -1,5 +1,5 @@
 PRJ_NAME="rhpam7-case-mgmt"
-PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Order IT Hardware Demo"
+PRJ_DESCRIPTION="Red Hat Credit Card Fraud Detection Demo"
 
 #GitHub tag referencing the image streams and templates.
 OPENSHIFT_PAM7_TEMPLATES_TAG=7.3.1.GA
